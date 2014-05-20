48b6a67e2e5b8fe03c00dba08aeb2419282d2985
Zhitao Gong <zhitaao.gong@gmail.com>
2014-05-19 21:58:26 -0500

    pagination

--- js/common.js
+++ js/common.js
@@ -3,26 +3,75 @@
 // Example 8
 // https://developer.mozilla.org/en-US/docs/Web/API/Window.location
 
-function UrlParam(sSearch) {
-  var rNull = /^\s*$/, rBool = /^(true|false)$/i;
-  function buildValue(sValue) {
-    if (rNull.test(sValue)) return null;
-    if (rBool.test(sValue)) return sValue.toLowerCase() === "true";
-    if (isFinite(sValue)) return parseFloat(sValue);
-    if (isFinite(Date.parse(sValue))) return new Date(sValue);
-    return sValue;
+function UrlParam(url) {
+
+  "use strict";
+
+  var rNull = /^\s*$/,
+      rBool = /^(true|false)$/i;
+  function conv(val) {
+    if (rNull.test(val)) return null;
+    if (rBool.test(val)) return val.toLowerCase() === "true";
+    if (isFinite(val)) return parseFloat(val);
+    if (isFinite(Date.parse(val))) return new Date(val);
+    return val;
   }
-  if (sSearch.length > 1) {
-    for (var aItKey, nKeyId = 0,
-             aCouples = sSearch.substr(1).split("&");
-         nKeyId < aCouples.length; nKeyId++) {
-      aItKey = aCouples[nKeyId].split("=");
-      this[unescape(aItKey[0])] = aItKey.length > 1
-                                ? buildValue(unescape(aItKey[1]))
-                                : null;
-    }
-  }
-};
+
+  if (!url) return null;
+
+  return url.split("?")[1].split("&")
+      .reduce(function(acc, cur) {
+        var pair = cur.split("=");
+        acc[unescape(pair[0])] = pair.length > 1
+                                    ? conv(unescape(pair[1]))
+                                    : null;
+        return acc;
+      }, {});
+}
+
+// Credits:
+// https://github.com/eclipse/egit-github/blob/master/org.eclipse.egit.github.core/src/org/eclipse/egit/github/core/client/PageLinks.java#L43-75
+// https://github.com/thlorenz/parse-link-header
+function PageLink(linkHeader) {
+
+  if (!linkHeader)
+    return null;
+
+  return linkHeader
+    .split(',')
+    .reduce(function(acc, cur) {
+      var segments = cur.split(';');
+      if (segments.length < 2)
+        return acc;
+
+      var link = segments[0].trim();
+      if (link[0] !== '<' || link[link.length-1] !== '>')
+        return acc;
+      link = link.substring(1, link.length - 1);
+
+      var param = UrlParam(link);
+
+      for (var i = 1; i < segments.length; ++i) {
+
+        var rel = segments[i].trim().split('=');
+        if (rel.length < 2 || rel[0] !== 'rel')
+          continue;
+
+        var val = rel[1];
+        if (val[0] === '"' && val[val.length-1] === '"')
+          val = val.substring(1, val.length - 1);
+
+        acc[val] = {
+          'page': param.page ? param.page : 0,
+          'per_page': param.per_page ? param.per_page : 0,
+          'rel': val,
+          'url': link
+        };
+      }
+
+      return acc;
+    }, {});
+}
 
 var Home = 'http://gist.gongzhitaao.org';
 // var Home = 'http://0.0.0.0:4000';
--- js/get_gist_list.js
+++ js/get_gist_list.js
@@ -4,29 +4,23 @@
 
   // var viewurl = 'https://js.gongzhitaao.org/gist/?';
   var viewurl = Home + '/gist/?';
-  var gists = null;
-  var CountPerRefresh = 50;
+  var PerPage = 30;
+  var username = null;
+  var next = 1;
 
-  $(document).ready(function() {
-    var param = new UrlParam(window.location.search);
-    var username = param.user ? param.user : "gongzhitaao";
-
-    $('.user')
-      .attr('href', 'https://github.com/' + username)
-      .append(username);
-
+  function more_gist(page, per_page) {
     // Retrieve all gists, but only show part of them at first.
     $.ajax({
       type: 'GET',
-      url: GistAPI + '/users/' + username + '/gists',
+      url: GistAPI + '/users/' + username +
+          '/gists?page='+ page + '&per_page=' + PerPage.toString(),
       dataType: 'json',
       success: function(data, status, xhr) {
-        gists = data;
-        var $gist = $('.gists').html('');
-        var count = data.length < CountPerRefresh
-              ? data.length
-              : CountPerRefresh;
-        for (var i = 0; i < count; ++i) {
+        var link = PageLink(xhr.getResponseHeader('link'));
+        next = (link && ('next' in link)) ? link.next.page : 0;
+
+        var $gist = $('.gists');
+        for (var i = 0; i < data.length; ++i) {
           // if there is a `README.md', I assume that you want to list
           // this gist.
           if ('README.md' in data[i].files) {
@@ -35,26 +29,46 @@
                       $.param({id: data[i].url.substr(
                           data[i].url.lastIndexOf('/') + 1)})
             })
-              .addClass('gist')
-              .append($('<span>')
-                      .addClass('description')
-                      .text(data[i].description))
-              .css('background', 'url(' + GistRaw +
-                   username + '/' + data[i].id + '/raw/thumbnail.png'+ ') no-repeat center left')
-              .appendTo($gist);
+            .addClass('gist')
+            .append($('<span>')
+                    .addClass('description')
+                    .text(data[i].description))
+            .css('background', 'url(' + GistRaw +
+                 username + '/' + data[i].id +
+                 '/raw/thumbnail.png'+ ') no-repeat center left')
+            .appendTo($gist);
           }
+        }
+
+        $('.refresher').removeClass('fa-spin');
+
+        if (next <= 0) {
+          $('.refresher')
+            .removeClass('fa-refresh')
+            .addClass('fa-smile-o')
+            .unbind('click')
+            .text(' That\'s all')
+            .css('cursor', 'default');
         }
       }
     });
+  }
 
-    // Show more as scrolling down.
-    // $(window).scroll(function() {
+  $(document).ready(function() {
 
-    //   if ($(window).scrollTop() ==
-    //       $(document).height() - $(window).height()) {
-    //     console.log("helo");
-    //   }
-    // });
+    var param = UrlParam(window.location.search);
+    username = (param && param.user) ? param.user : "gongzhitaao";
+
+    $('.user')
+      .attr('href', 'https://github.com/' + username)
+      .append(username);
+
+    more_gist(next, PerPage);
+
+    $('.refresher').click(function() {
+      $(this).addClass('fa-spin');
+      more_gist(next, PerPage);
+    });
   });
 
 })(jQuery);
